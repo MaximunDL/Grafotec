9ffeca803fd709943ab977b8339be89c3a1a33c4
maximundl <diegodaliosorio@gmail.com>
2023-02-11 17:44:42 -0500

    1.2.1

---
+++ inicio/static/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.banner', {delay: 250} );
+ScrollReveal().reveal('.info', { delay: 250} );
+ScrollReveal().reveal('.contenedor-cards', { delay: 250});
+ScrollReveal().reveal('.servicio', { delay: 350});
+ScrollReveal().reveal('.clientes-img', { delay: 250});
