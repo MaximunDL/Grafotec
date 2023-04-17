261b05f6e5f65da3a9b76935d14068e0818d5242
maximundl <diegodaliosorio@gmail.com>
2023-04-17 10:39:19 -0500

    last_change

--- inicio/static/main.js
+++ inicio/static/main.js
@@ -1,7 +1,8 @@
 ScrollReveal().reveal('.banner', { delay: 250 });
-ScrollReveal().reveal('.info', { delay: 250 });
+ScrollReveal().reveal('.smallimg', { delay: 250 });
 ScrollReveal().reveal('.contenedor-cards', { delay: 250 });
 ScrollReveal().reveal('.servicio', { delay: 350 });
+ScrollReveal().reveal('.clientes-img1', { delay: 250 });
 ScrollReveal().reveal('.clientes-img', { delay: 250 });
 ScrollReveal().reveal('.Contactenos-te', { delay: 250 });
 ScrollReveal().reveal('.Contactenos-et', { delay: 250 });
@@ -12,11 +13,34 @@ ScrollReveal().reveal('.pilares', { delay: 250 });
 let images = document.querySelectorAll('.banner-images-container img');
 let index = 0;
 let bannerText = document.querySelector('.banner-text');
-let textArray = ['IMPRESIONES DE <br> ALTA CALIDAD', 'TRANSFORMAMOS TUS <br>IDEAS EN <br>IMPRESIONES ÚNICAS', 'DEJA TODO EN <br> NUESTRAS MANOS'];
+let subText = document.querySelector('.banner-subtext');
+let btnText = document.querySelector('.btn');
+let textArray = [
+  {
+    heading: 'IMPRESIONES DE <br> ALTA CALIDAD',
+    subheading: 'Profesionales en la producción.',
+    buttonLink: '{% url "servicio" %}',
+    buttonText: 'NUESTROS SERVICIOS',
+  },
+  {
+    heading: 'TUS IDEAS <br> EN IMPRESIONES <br> ÚNICAS',
+    subheading: 'Impresiones personalizadas.',
+    buttonLink: '{% url "about" %}',
+    buttonText: 'CONOCENOS',
+  },
+  {
+    heading: 'DEJA TODO EN <br> NUESTRAS MANOS',
+    subheading: 'Nos encargamos de principio a <br> fin en la realización de tus proyectos.',
+    buttonLink: '{% url "contact" %}',
+    buttonText: 'CONSIGUE UNA COTIZACIÓN',
+  }
+];
 
 setInterval(function () {
   images[index].classList.remove('active');
-  bannerText.innerHTML = textArray[index];
+  bannerText.innerHTML = textArray[index].heading ;
+  subText.innerHTML = textArray[index].subheading;
+  btnText.innerHTML = textArray[index].buttonText;
   index = (index + 1) % images.length;
   images[index].classList.add('active');
 }, 5000);
